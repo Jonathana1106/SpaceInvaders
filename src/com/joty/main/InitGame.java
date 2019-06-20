08f3acd2e326240c9e0f49dd1b612522468ba867
Jonathan G Araya <jonathana1196@gmail.com>
2019-06-20 02:52:08 -0600

    Initial commit

---
+++ src/com/joty/main/InitGame.java
@@ -0,0 +1,20 @@
+package com.joty.main;
+/**
+ * 
+ */
+
+/**
+ * @author Jonathan
+ *
+ */
+public class InitGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
